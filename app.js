6e12d372d8e2fdb0bf2e068392b197dbc5d13708
tbenitz <thomas.allan.stone@gmail.com>
2026-09-16 18:49:08 -0600

    Add in-browser app loader

---
+++ app.js
@@ -0,0 +1,8 @@
+const parts = await Promise.all(
+  [1, 2, 3, 4].map((n) => fetch(`./p${n}.js.txt`).then((r) => {
+    if (!r.ok) throw new Error(`Missing p${n}.js.txt`);
+    return r.text();
+  }))
+);
+const url = URL.createObjectURL(new Blob(parts, { type: "text/javascript" }));
+await import(url);
